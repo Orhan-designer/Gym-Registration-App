a7c5f57b7fbb85d8d100de386caf569130ee0bf0
Orhan <ya.eskimo-callboy@yandex.by>
2023-04-10 15:33:32 +0300

    feat: register ui

--- gym-registration/src/app/app.module.ts
+++ gym-registration/src/app/app.module.ts
@@ -5,6 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -23,9 +29,16 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- gym-registration/src/app/registration/registration.component.ts
+++ gym-registration/src/app/registration/registration.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  public packages: string[] = ['Monthly', 'Quarterly', 'Yearly'];
+  public genders: string[] = ['Male', 'Female'];
+  public importantList: string[] = [
+    'Toxic Fat reduction',
+    'Energy and Endurance',
+    'Building Lean Muscle',
+    'Healthier Digestive System',
+    'Sugar Craving Body',
+    'Fitness'
+  ];
 
   constructor() { }
 
